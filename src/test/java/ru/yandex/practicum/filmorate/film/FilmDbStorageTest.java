a04cc5deb91e6357cae0fa9fc396df375318ff2f
Evgeniy <valikaev1989@yandex.ru>
2022-07-16 17:14:44 +0300

    try pr1

--- src/test/java/ru/yandex/practicum/filmorate/film/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/film/FilmDbStorageTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.models.Film;
