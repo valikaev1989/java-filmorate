4e472d2c5f757cf6bd9f764ef8590d588efb0e43
Evgeniy <valikaev1989@yandex.ru>
2022-07-16 17:50:46 +0300

    try pr2

--- src/main/java/ru/yandex/practicum/filmorate/models/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/models/Genre.java
@@ -13,30 +13,30 @@ public class Genre {
         this.id = id;
         this.name = name;
     }
-    public Genre(int id) {
-        this.id = id;
-
-        switch (id) {
-            case 1:
-                name = "Комедия";
-                break;
-            case 2:
-                name = "Драма";
-                break;
-            case 3:
-                name = "Мультфильм";
-                break;
-            case 4:
-                name = "Триллер";
-                break;
-            case 5:
-                name = "Документальный";
-                break;
-            case 6:
-                name = "Боевик";
-                break;
-            default:
-                throw new ValidationException("Такого Id нет");
-        }
-    }
+//    public Genre(int id) {
+//        this.id = id;
+//
+//        switch (id) {
+//            case 1:
+//                name = "Комедия";
+//                break;
+//            case 2:
+//                name = "Драма";
+//                break;
+//            case 3:
+//                name = "Мультфильм";
+//                break;
+//            case 4:
+//                name = "Триллер";
+//                break;
+//            case 5:
+//                name = "Документальный";
+//                break;
+//            case 6:
+//                name = "Боевик";
+//                break;
+//            default:
+//                throw new ValidationException("Такого Id нет");
+//        }
+//    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/models/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/models/Mpa.java
@@ -12,26 +12,26 @@ public class Mpa {
         this.id = id;
         this.name = name;
     }
-    public Mpa(int id) {
-        this.id = id;
-        switch (id){
-            case 1:
-                name = "G";
-                break;
-            case 2:
-                name = "PG";
-                break;
-            case 3:
-                name = "PG-13";
-                break;
-            case 4:
-                name = "R";
-                break;
-            case 5:
-                name = "NC-17";
-                break;
-            default:
-                throw new ValidationException("Такого ID нет");
-        }
-    }
+//    public Mpa(int id) {
+//        this.id = id;
+//        switch (id){
+//            case 1:
+//                name = "G";
+//                break;
+//            case 2:
+//                name = "PG";
+//                break;
+//            case 3:
+//                name = "PG-13";
+//                break;
+//            case 4:
+//                name = "R";
+//                break;
+//            case 5:
+//                name = "NC-17";
+//                break;
+//            default:
+//                throw new ValidationException("Такого ID нет");
+//        }
+//    }
 }
